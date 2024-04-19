be67b46bf58d6c666be412e36e107700e083c355
timmushen <tim.mushen@northwestu.edu>
2024-04-18 20:37:14 -0700

    hero

--- app.vue
+++ app.vue
@@ -7,4 +7,7 @@
 .roboto {
   font-family: Roboto, sans-serif;
 }
+.btn{
+  @apply bg-flamingo-600 px-4 py-3 text-center text-sm font-semibold inline-block text-white cursor-pointer uppercase transition duration-200 ease-in-out rounded-md hover:bg-indigo-700 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-600 focus-visible:ring-offset-2 active:scale-95
+}
 </style>
--- components/Footer.vue
+++ components/Footer.vue
@@ -9,5 +9,21 @@
         <nuxt-link to="/page3">Page 3</nuxt-link>
         <nuxt-link to="/page4">Page 4</nuxt-link>
     </nav>
+
+    <div>
+        <div class="btn">Hi</div>
+      
+    </div>
+    <div>
+        <nuxt-link to="/page4">  
+            <button class="bg-sky-600 text-white px-6 py-2">Say Hi</button>
+        </nuxt-link>
+      
+    </div>
+    <div>
+        <a href="https://microsoft.com">  
+            <button class="bg-sky-600 text-white px-6 py-2">Go to Microsoft</button>
+        </a>
+    </div>
    </footer>
 </template>
