49e38cb1789e3f7d76819b1ff08572272dd87097
timmushen <tim.mushen@northwestu.edu>
2024-04-18 19:56:31 -0700

    Header & Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,13 @@
 <template>
-    Footer Here
+   <footer class="bg-gray-200 border-t  text-center border-gray-300 p-8">
+    <div class="mb-6 text-lg"> 
+        <nuxt-link to="/">Site Name</nuxt-link>
+    </div>
+    <nav class="flex flex-row justify-center gap-4">
+        <nuxt-link to="/page1">Page 1</nuxt-link>
+        <nuxt-link to="/page2">Page 2</nuxt-link>
+        <nuxt-link to="/page3">Page 3</nuxt-link>
+        <nuxt-link to="/page4">Page 4</nuxt-link>
+    </nav>
+   </footer>
 </template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,15 @@
 <template>
-    Header & Nav Here
+    <header class="bg-sky-600 py-2">
+        <main class="container mx-auto flex flex-row justify-between">
+            <div class="text-white text-lg font-bold">
+                <nuxt-link to="/">Site Name</nuxt-link>
+            </div>
+            <nav class="flex flex-row justify-end gap-4">
+                <nuxt-link to="/page1">Page 1</nuxt-link>
+                <nuxt-link to="/page2">Page 2</nuxt-link>
+                <nuxt-link to="/page3">Page 3</nuxt-link>
+                <nuxt-link to="/page4">Page 4</nuxt-link>
+            </nav>
+        </main>
+    </header>
 </template>
